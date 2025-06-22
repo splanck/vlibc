fb8d189a44a92099cf8d193bec10bf5f5ccd0393
Stephen Planck <splanck9271@gmail.com>
2025-06-22 00:28:09 -0400

    Rewrite dirent to use system readdir

--- include/dirent.h
+++ include/dirent.h
@@ -4,24 +4,29 @@
 #include <sys/types.h>
 #include <stddef.h>
 
+/* BSD style directory entry */
 struct dirent {
-    ino_t d_ino;
-    off_t d_off;
-    unsigned short d_reclen;
-    unsigned char d_type;
-    char d_name[256];
+    ino_t          d_fileno;   /* file number */
+    off_t          d_off;      /* seek offset to next entry */
+    unsigned short d_reclen;   /* length of this record */
+    unsigned char  d_type;     /* file type */
+    unsigned char  d_namlen;   /* length of name */
+    char           d_name[256];/* null terminated file name */
 };
 
+#define d_ino d_fileno
+
 typedef struct {
-    int fd;
-    size_t buf_pos;
-    size_t buf_len;
-    char buf[512];
-    struct dirent ent;
+    void *impl;               /* pointer to system DIR structure */
+    struct dirent ent;        /* storage for returned entry */
 } DIR;
 
-DIR *opendir(const char *name);
-struct dirent *readdir(DIR *dirp);
-int closedir(DIR *dirp);
+DIR *vlibc_opendir(const char *name);
+struct dirent *vlibc_readdir(DIR *dirp);
+int vlibc_closedir(DIR *dirp);
+
+#define opendir  vlibc_opendir
+#define readdir  vlibc_readdir
+#define closedir vlibc_closedir
 
 #endif /* DIRENT_H */
--- src/dirent.c
+++ src/dirent.c
@@ -1,27 +1,26 @@
+#define _GNU_SOURCE
+#include <dirent.h>
 #include "dirent.h"
+#undef opendir
+#undef readdir
+#undef closedir
 #include "memory.h"
 #include "io.h"
 #include "errno.h"
-#include "string.h"
+#include <string.h>
 #include <fcntl.h>
 #ifndef O_DIRECTORY
 #define O_DIRECTORY 0200000
 #endif
-#include <sys/syscall.h>
-#include <stdint.h>
-#include <stddef.h>
-#include <unistd.h>
-#include "syscall.h"
 
-struct linux_dirent64 {
-    uint64_t d_ino;
-    int64_t  d_off;
-    unsigned short d_reclen;
-    unsigned char  d_type;
-    char d_name[];
-};
+typedef struct __dirstream sys_DIR;
 
-DIR *opendir(const char *name)
+/* Declare the system directory functions we wrap. */
+extern DIR *fdopendir(int);
+extern struct dirent *readdir(sys_DIR *);
+extern int closedir(sys_DIR *);
+
+DIR *vlibc_opendir(const char *name)
 {
     int fd = open(name, O_RDONLY | O_DIRECTORY);
     if (fd < 0)
@@ -33,53 +32,32 @@ DIR *opendir(const char *name)
         errno = ENOMEM;
         return NULL;
     }
-    d->fd = fd;
-    d->buf_pos = 0;
-    d->buf_len = 0;
+    d->impl = fdopendir(fd);
+    if (!d->impl) {
+        close(fd);
+        free(d);
+        return NULL;
+    }
     return d;
 }
 
-struct dirent *readdir(DIR *dirp)
+struct dirent *vlibc_readdir(DIR *dirp)
 {
-    if (!dirp)
+    if (!dirp || !dirp->impl)
         return NULL;
 
-    if (dirp->buf_pos >= dirp->buf_len) {
-        long ret = vlibc_syscall(SYS_getdents64, dirp->fd,
-                                 (long)dirp->buf, sizeof(dirp->buf), 0, 0, 0);
-        if (ret < 0) {
-            errno = -ret;
-            return NULL;
-        }
-        if (ret == 0)
-            return NULL;
-        dirp->buf_len = (size_t)ret;
-        dirp->buf_pos = 0;
-    }
-
-    struct linux_dirent64 *ld = (struct linux_dirent64 *)(dirp->buf + dirp->buf_pos);
-    dirp->buf_pos += ld->d_reclen;
-
-    dirp->ent.d_ino = (ino_t)ld->d_ino;
-    dirp->ent.d_off = (off_t)ld->d_off;
-    dirp->ent.d_reclen = ld->d_reclen;
-    dirp->ent.d_type = ld->d_type;
-
-    size_t name_len = ld->d_reclen - offsetof(struct linux_dirent64, d_name);
-    if (name_len >= sizeof(dirp->ent.d_name))
-        name_len = sizeof(dirp->ent.d_name) - 1;
-    vmemcpy(dirp->ent.d_name, ld->d_name, name_len);
-    dirp->ent.d_name[name_len] = '\0';
-
+    struct dirent *e = readdir((sys_DIR *)dirp->impl);
+    if (!e)
+        return NULL;
+    memcpy(&dirp->ent, e, sizeof(struct dirent));
     return &dirp->ent;
 }
 
-int closedir(DIR *dirp)
+int vlibc_closedir(DIR *dirp)
 {
     if (!dirp)
         return -1;
-    int r = close(dirp->fd);
+    int r = closedir((sys_DIR *)dirp->impl);
     free(dirp);
     return r;
 }
-
