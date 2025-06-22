4594ddc74698ba9600c3704a07aeeb4bf9c05671
Stephen Planck <splanck9271@gmail.com>
2025-06-22 01:49:08 -0400

    use system dirent

--- src/dirent.c
+++ src/dirent.c
@@ -4,60 +4,18 @@
 #undef opendir
 #undef readdir
 #undef closedir
-#include "memory.h"
-#include "io.h"
-#include "errno.h"
-#include <string.h>
-#include <fcntl.h>
-#ifndef O_DIRECTORY
-#define O_DIRECTORY 0200000
-#endif
-
-typedef struct __dirstream sys_DIR;
-
-/* Declare the system directory functions we wrap. */
-extern DIR *fdopendir(int);
-extern struct dirent *readdir(sys_DIR *);
-extern int closedir(sys_DIR *);
 
 DIR *vlibc_opendir(const char *name)
 {
-    int fd = open(name, O_RDONLY | O_DIRECTORY);
-    if (fd < 0)
-        return NULL;
-
-    DIR *d = malloc(sizeof(DIR));
-    if (!d) {
-        close(fd);
-        errno = ENOMEM;
-        return NULL;
-    }
-    d->impl = fdopendir(fd);
-    if (!d->impl) {
-        close(fd);
-        free(d);
-        return NULL;
-    }
-    return d;
+    return opendir(name);
 }
 
 struct dirent *vlibc_readdir(DIR *dirp)
 {
-    if (!dirp || !dirp->impl)
-        return NULL;
-
-    struct dirent *e = readdir((sys_DIR *)dirp->impl);
-    if (!e)
-        return NULL;
-    memcpy(&dirp->ent, e, sizeof(struct dirent));
-    return &dirp->ent;
+    return readdir(dirp);
 }
 
 int vlibc_closedir(DIR *dirp)
 {
-    if (!dirp)
-        return -1;
-    int r = closedir((sys_DIR *)dirp->impl);
-    free(dirp);
-    return r;
+    return closedir(dirp);
 }
