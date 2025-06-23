db9696d0a91bba212a348f3c18bc061d8a992c1f
Stephen Planck <splanck9271@gmail.com>
2025-06-22 23:41:18 -0400

    Add getopt_long_only support and tests

--- include/getopt.h
+++ include/getopt.h
@@ -21,5 +21,7 @@ struct option {
 int getopt(int argc, char * const argv[], const char *optstring);
 int getopt_long(int argc, char * const argv[], const char *optstring,
                const struct option *longopts, int *longindex);
+int getopt_long_only(int argc, char * const argv[], const char *optstring,
+                     const struct option *longopts, int *longindex);
 
 #endif /* GETOPT_H */
