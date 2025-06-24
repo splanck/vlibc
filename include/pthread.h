4193e413d7e7e32200ab91b5ac4fbb5655a44190
Stephen Planck <splanck9271@gmail.com>
2025-06-23 22:17:19 -0400

    Add pthread_self and pthread_equal

--- include/pthread.h
+++ include/pthread.h
@@ -36,6 +36,8 @@ int pthread_create(pthread_t *thread, const void *attr,
                    void *(*start_routine)(void *), void *arg);
 int pthread_join(pthread_t thread, void **retval);
 int pthread_detach(pthread_t thread);
+pthread_t pthread_self(void);
+int pthread_equal(pthread_t a, pthread_t b);
 
 int pthread_mutex_init(pthread_mutex_t *mutex, void *attr);
 int pthread_mutex_destroy(pthread_mutex_t *mutex);
